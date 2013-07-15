73b8172997f8bf7373b4f8a1109a36c495c0b476
Alexey Shmalko <rasen.dubi@gmail.com>
2013-07-15 11:58:03 +0300

    Fix: initialize and destroy vertex_normals field

--- src/objmodel.c
+++ src/objmodel.c
@@ -8,12 +8,15 @@ obj_model *obj_model_new() {
 			sizeof(geometric_vertex));
 	model->texture_vertices = g_array_new(FALSE, FALSE,
 			sizeof(texture_vertex));
+	model->vertex_normals = g_array_new(FALSE, FALSE,
+			sizeof(vertex_normal));
 	return model;
 }
 
 void obj_model_free(obj_model *model) {
 	g_array_free(model->geometric_vertices, TRUE);
 	g_array_free(model->texture_vertices, TRUE);
+	g_array_free(model->vertex_normals, TRUE);
 	free(model);
 }
 
