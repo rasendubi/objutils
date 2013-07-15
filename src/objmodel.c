8dd447ecb8d071ab1d0b499e86abbd39e74fae8c
Alexey Shmalko <rasen.dubi@gmail.com>
2013-07-15 11:38:32 +0300

    Add vertex normals

--- src/objmodel.c
+++ src/objmodel.c
@@ -2,14 +2,6 @@
 
 #include <stdlib.h>
 
-size_t obj_n_geometric_vertices(obj_model *model) {
-	return model->geometric_vertices->len;
-}
-
-size_t obj_n_texture_vertices(obj_model *model) {
-	return model->texture_vertices->len;
-}
-
 obj_model *obj_model_new() {
 	obj_model *model = malloc(sizeof(obj_model));
 	model->geometric_vertices = g_array_new(FALSE, FALSE,
--- src/objmodel.h
+++ src/objmodel.h
@@ -11,18 +11,33 @@ typedef struct texture_vertex {
 	double u, v, w;
 } texture_vertex;
 
+typedef struct vertex_normal {
+	double i, j, k;
+} vertex_normal;
+
 typedef struct obj_model {
 	GArray *geometric_vertices;
 	GArray *texture_vertices;
+	GArray *vertex_normals;
 } obj_model;
 
 #define obj_geometric_vertex(model, i) \
 	g_array_index((model)->geometric_vertices, geometric_vertex, i)
-extern size_t obj_n_geometric_vertices(obj_model *);
+static inline size_t obj_n_geometric_vertices(obj_model *model) {
+	return model->geometric_vertices->len;
+}
 
 #define obj_texture_vertex(model, i) \
 	g_array_index((model)->texture_vertices, texture_vertex, i)
-extern size_t obj_n_texture_vertices(obj_model *);
+static inline size_t obj_n_texture_vertices(obj_model *model) {
+	return model->texture_vertices->len;
+}
+
+#define obj_vertex_normal(model, i) \
+	g_array_index((model)->vertex_normals, vertex_normal, i)
+static inline size_t obj_n_vertex_normals(obj_model *model) {
+	return model->vertex_normals->len;
+}
 
 extern obj_model *obj_model_new();
 extern void obj_model_free(obj_model *);
