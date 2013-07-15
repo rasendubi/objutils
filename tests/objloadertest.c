c5dd70dc3c30e4184acbc436ebba7c87ef5c4e05
Alexey Shmalko <rasen.dubi@gmail.com>
2013-07-15 11:50:22 +0300

    Add vertex normals test

--- tests/objloadertest.c
+++ tests/objloadertest.c
@@ -16,6 +16,13 @@ void assert_texture_vertex_eq(const texture_vertex v1,
 
 }
 
+void assert_vertex_normal_eq(const vertex_normal v1,
+		const vertex_normal v2) {
+	g_assert_cmpfloat(v1.i, ==, v2.i);
+	g_assert_cmpfloat(v1.j, ==, v2.j);
+	g_assert_cmpfloat(v1.k, ==, v2.k);
+}
+
 void test_geometric_vertices(void) {
 	char *test_data[] = {
 		"v 1.02 3 5.06",
@@ -81,12 +88,46 @@ void test_texture_vertices(void) {
 	obj_model_free(model);
 }
 
+void test_vertex_normals(void) {
+	char *test_data[] = {
+		"  vn\t 1.2 3.4 5.6",
+		"vn  4 5 -6e-017"
+	};
+	vertex_normal answer[] = {
+		{ 1.2, 3.4, 5.6 },
+		{ 4, 5, -6e-017 }
+	};
+	const unsigned int N = sizeof(test_data)/sizeof(test_data[0]);
+
+	obj_model *model = obj_model_new();
+
+	for (int i = 0; i < N; ++i) {
+		g_assert(parse_line(model, test_data[i]));
+	}
+
+	g_assert(!parse_line(model, "vn 1 2.3"));
+	g_assert(!parse_line(model, "vn 1"));
+	g_assert(!parse_line(model, "vn"));
+	g_assert(!parse_line(model, "vn 4 5 6 7"));
+
+	g_assert_cmpuint(obj_n_vertex_normals(model), ==, N);
+
+	for (int i = 0; i < N; ++i) {
+		assert_vertex_normal_eq(obj_vertex_normal(model, i),
+				answer[i]);
+	}
+
+	obj_model_free(model);
+}
+
 int main(int argc, char *argv[]) {
 	g_test_init(&argc, &argv, NULL);
 	g_test_add_func("/objloader/vertices/geometric",
 			test_geometric_vertices);
 	g_test_add_func("/objloader/vertices/texture",
 			test_texture_vertices);
+	g_test_add_func("/objloader/vertices/normals",
+			test_vertex_normals);
 	return g_test_run();
 }
 
